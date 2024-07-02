ba04a4594e5ae0ef471f02d5431dd06ba9a0fb56
Dipesh Khanal <dipeshkhanal79@gmail.com>
2024-07-02 23:31:42 +0545

    feat: url added in config

--- src/config/cybersource.php
+++ src/config/cybersource.php
@@ -4,4 +4,5 @@ return array(
     'secret_key' => env('CYBERSOURCE_SECRET_KEY'),
     'access_key' => env('CYBERSOURCE_ACCESS_KEY'),
     'profile_id' => env('CYBERSOURCE_PROFILE_ID'),
+    'url'=> env('CYBERSOURCE_URL'),
 );
